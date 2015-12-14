b09386f7c8ffc3014730dc59af1828f4dd30202a
Nikolai Sitnikov <nikolai.sitnikov@ramax.ru>
2015-12-14 09:31:25 +0300

    Initial commit

---
+++ src/main/java/ru/ramax/processmining/App.java
@@ -0,0 +1,17 @@
+package ru.ramax.processmining;
+
+import org.deckfour.xes.model.XLog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        XLog outputLog;
+    }
+}
