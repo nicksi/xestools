f5c6087175a952679bcb076f1c83932b361e0dbc
Nikolai Sitnikov <nikolai.sitnikov@gmail.com>
2015-12-16 15:27:25 +0300

    New function prototypes

--- src/main/java/ru/ramax/processmining/xestools.java
+++ src/main/java/ru/ramax/processmining/xestools.java
@@ -22,19 +22,25 @@ public class XEStools {
 
     // cache for traceStartTime
     private Map<String, LocalDateTime> traceStartTime = Maps.newHashMap();
+    // cache to search for trace by concept:name
+    private Map<String, Integer> name2index = Maps.newHashMap();
 
     public XEStools(@NonNull XLog xlog) {
         this.xlog = xlog;
 
     }
 
+    /***
+     * Select log to work with and clear cache
+     * @param xLog
+     */
     public void setXLog(@NonNull XLog xLog) {
         this.xlog = xLog;
         clearCache();
     }
 
     /***
-     * Find the start timestamp of the log
+     * Find the start timestamp of the trace
      * @param xTrace
      * @return LocalDateTime
      */
@@ -47,7 +53,7 @@ public class XEStools {
         }
         else {
             if (xTrace.size() > 0) {
-                XEvent xEvent = xTrace.get(0);
+                XEvent xEvent = xTrace.get(0); // TODO switch to minimal date time, not first
                 if (xEvent != null && xEvent.hasAttributes()) {
                     XAttributeMap xAttributeMap = xEvent.getAttributes();
                     for (XAttribute attribute : xAttributeMap.values()) {
@@ -64,22 +70,119 @@ public class XEStools {
         return startTime;
     }
 
+    /***
+     * Find the start timestamp of the trace based on trace concept:name
+     * @param index Concept name to find
+     * @return
+     */
     public LocalDateTime traceStartTime(@NonNull String index) {
         LocalDateTime startTime = LocalDateTime.MIN;
 
-        Iterator xTraceIterator = xlog.iterator();
-        while (xTraceIterator.hasNext()) {
-            XTrace xTrace = (XTrace) xTraceIterator.next();
-
-            if (getIndex(xTrace).equals(index)) {
-                startTime = traceStartTime(xTrace);
-                break;
-            }
-
+        XTrace xTrace = getXTrace(index);
+        if (xTrace != null)
+        {
+            startTime = traceStartTime(xTrace);
         }
 
         return startTime;
     }
+
+    /***
+     * Find the last timestamp of the trace based on trace concept:name
+     * @param xTrace
+     * @return
+     */
+    public LocalDateTime traceEndTime(@NonNull XTrace xTrace) {
+        LocalDateTime endTime = LocalDateTime.MAX;
+
+        return  endTime;
+    }
+
+    /***
+     * Find the last timestamp of the trace based on trace concept:name
+     * @param index Concept name to find
+     * @return
+     */
+    public LocalDateTime traceStartTime(@NonNull String index) {
+        LocalDateTime endTime = LocalDateTime.MAX;
+
+        XTrace xTrace = getXTrace(index);
+        if (xTrace != null)
+        {
+            endTime = traceEndTime(xTrace);
+        }
+
+        return endTime;
+    }
+
+    /***
+     * Find the duration of trace
+     * @param xTrace
+     * @return
+     */
+    public Long getTraceDuration(@NonNull XTrace xTrace) {
+        Long duration = 0L;
+
+        LocalDateTime start = traceEndTime(xTrace);
+        LocalDateTime end = traceStartTime(xTrace);
+
+        return duration;
+    }
+
+
+    /***
+     * Find the duration of trace based on trace concept:name
+     * @param index Concept name to find trace
+     * @return
+     */
+    public Long getTraceDuration(@NonNull String index) {
+        Long duration = 0L;
+
+        XTrace xTrace = getXTrace(index);
+        if (xTrace != null)
+        {
+            duration = getTraceDuration(xTrace);
+        }
+
+        return duration;
+    }
+
+    public Map<String, Long> getDurations(String filter) {
+
+    }
+
+
+    /***
+     * Search for trace by it's concept:name
+     * @param name
+     * @return
+     */
+    public XTrace getXTrace(String name) {
+        XTrace xTrace = null;
+        int i = 0;
+
+        if (name2index.containsKey(name))
+            xTrace = xlog.get(name2index.get(name));
+        else {
+            Iterator xTraceIterator = xlog.iterator();
+            while (xTraceIterator.hasNext()) {
+                XTrace currentTrace = (XTrace) xTraceIterator.next();
+
+                if (getIndex(currentTrace).equals(name)) {
+                    // TODO not first but minimal
+                    xTrace = currentTrace;
+                    name2index.put(name, i);
+                    break;
+                }
+
+                i++;
+            }
+        }
+
+        return xTrace;
+    }
+
+    /* Private functions */
 
     /***
      * Clear all cache maps
